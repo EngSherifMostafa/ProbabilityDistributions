40bd79e06ce8201a034e8f49bfe955008511f0a4
ahmed-sherif24 <102686773+ahmed-sherif24@users.noreply.github.com>
2023-12-15 18:15:09 +0200

    Update Bernoulli.py

--- Discrete/Bernoulli.py
+++ Discrete/Bernoulli.py
@@ -1,6 +1,24 @@
 import numpy as np
 import scipy.stats as stats
-from plot import plot_pmf, plot_cdf
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_pmf(dis_type, title, x_axis, y_axis):
+    plt.title(dis_type+"\n"+title)
+    plt.xlabel('state')
+    plt.ylabel('Probability')
+    plt.bar(x_axis, y_axis)
+    plt.show()
+
+
+def plot_cdf(dis_type, title, x_axis, y_axis):
+    plt.title(dis_type+"\n"+title)
+    plt.xlabel('state')
+    plt.ylabel('Probability')
+    plt.step(x_axis, y_axis)
+    plt.show()
 
 p = 0.3
 rv = stats.bernoulli(p)
@@ -11,5 +29,20 @@ F = rv.cdf(x)
 mean, var = rv.stats(moments="mv")
 print(f"mean: {mean}\nVariance: {var}")
 
-plot_pmf(dis_type="Bernoulli", title='PMF', x_axis=x, y_axis=f)
+plt.subplot(121)
+plt.title("Probability Mass Function")
+
+plt.xlabel('x')
+plt.ylabel('f')
+plt.plot(x, f, "bo", ms=8, label="dddddd")
+plt.vlines(x, 0, f, colors="b", lw=5, alpha=0.5)
+    
+plt.subplot(122)
+plt.title("Histogram")
+plt.bar(x, f, align='center', alpha=0.7)
+plt.xlabel('x')
+plt.ylabel('f')
+plt.show()
+
+
 plot_cdf(dis_type="Bernoulli", title='CDF', x_axis=x, y_axis=F)
