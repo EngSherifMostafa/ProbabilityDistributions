c10f3e57d579eb7a3a75df94cae35d806e8bc099
AhmedEslam152 <eslamahmed23468@gmail.com>
2023-12-16 21:46:06 +0200

    berno

--- Discrete/Bernoulli.py
+++ Discrete/Bernoulli.py
@@ -1,41 +1,25 @@
 import numpy as np
 import scipy.stats as stats
-
-
 import matplotlib.pyplot as plt
 
-
 def plot_pmf(dis_type, title, x_axis, y_axis):
-    plt.title(dis_type+"\n"+title)
+    plt.title(dis_type + "\n" + title)
     plt.xlabel('state')
     plt.ylabel('Probability')
     plt.bar(x_axis, y_axis)
-    plt.show()
-
-
-def plot_cdf(dis_type, title, x_axis, y_axis):
-    plt.title(dis_type+"\n"+title)
-    plt.xlabel('state')
-    plt.ylabel('Probability')
-    plt.step(x_axis, y_axis)
     plt.show()
 
 p = 0.3
 rv = stats.bernoulli(p)
 x = np.linspace(0, 1, 2)
 f = rv.pmf(x)
-F = rv.cdf(x)
-
 mean, var = rv.stats(moments="mv")
 print(f"mean: {mean}\nVariance: {var}")
 
-
-plt.title("")
-
-
+plt.subplot(121)
+plt.title("Probability Mass Function")
+plt.xlabel('x')
+plt.ylabel('f')
 plt.plot(x, f, "bo", ms=8, label="dddddd")
 plt.vlines(x, 0, f, colors="b", lw=5, alpha=0.5)
-
 plt.show()
-
-
--- Discrete/Geometric.py
+++ Discrete/Geometric.py
@@ -1,13 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
+p = 0.5
 
-p = float(input("Enter the probability of success (p): "))
-random_variable = np.random.geometric(p)
-pmf = (1 - p) ** np.arange(random_variable) * p
-mean = np.mean(pmf)
-variance = np.var(pmf)
+# Calculate the mean and variance and printing them
+mean = stats.geom.mean(p)
+variance = stats.geom.var(p)
 
-print("Random Variable:", random_variable)
-print("PMF:", pmf)
-print("Mean:", mean)
-print("Variance:", variance)
+print(f"Mean: {mean}")
+print(f"Variance: {variance}")
+##########################################
+x = np.arange(1, 11)
+rv = stats.geom(p)
+
+# Calculate the(PMF) for the given x values and plotting it
+pmf = rv.pmf(x)
+
+plt.plot(x, pmf, 'bo', ms=8, label='PMF')
+plt.vlines(x, 0, pmf, colors='b', lw=5, alpha=0.5)
+plt.title('Probability Mass Function')
+plt.show()
+
+# Calculate the (CDF) for the given x values and plotting it
+cdf = rv.cdf(x)
+
+plt.step(x, cdf, where='post', color='r', label='CDF')
+plt.title('Cumulative Distribution Function')
+plt.show()
