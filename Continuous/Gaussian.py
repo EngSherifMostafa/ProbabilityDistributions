91f3e9fadd1c90609b4a24b1899f795307efa91e
rgamr <152027825+rgamr@users.noreply.github.com>
2023-12-17 20:12:00 +0200

    Update Gaussian.py

--- Continuous/Gaussian.py
+++ Continuous/Gaussian.py
@@ -1,12 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
-from plot import plot_pdf, plot_cdf
 
+# Set the parameters for the Gaussian distribution
+mu = 0      # Mean
+sigma = 1   # Standard deviation
 
-x = np.linspace(-10, 10, 1000)
-f = stats.norm.pdf(x)
-F = stats.norm.cdf(x)
+# Generate random variables from the Gaussian distribution
+sample_size = 1000
+random_vars = np.random.normal(loc=mu, scale=sigma, size=sample_size)
 
-plot_pdf(dis_type="Gaussian", title="PDF", x_axis=x, y_axis=f)
-plot_cdf(dis_type="Gaussian", title="CDF", x_axis=x, y_axis=F)
+# Calculate the mean and variance
+mean = np.mean(random_vars)
+variance = np.var(random_vars)
+
+# Plot the PDF
+x = np.linspace(mu - 4*sigma, mu + 4*sigma, 1000)
+pdf = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
+plt.plot(x, pdf, label='PDF')
+plt.xlabel('Random Variable')
+plt.ylabel('Probability Density')
+plt.title('Gaussian Continuous Distribution - PDF')
+plt.legend()
+plt.show()
+
+# Plot the CDF
+sorted_vars = np.sort(random_vars)
+cdf = np.linspace(0, 1, sample_size)
+plt.plot(sorted_vars, cdf, label='CDF')
+plt.xlabel('Random Variable')
+plt.ylabel('Cumulative Probability')
+plt.title('Gaussian Continuous Distribution - CDF')
+plt.legend()
+plt.show()
+
+# Print the mean and variance
+print("Mean:", mean)
+print("Variance:", variance)
