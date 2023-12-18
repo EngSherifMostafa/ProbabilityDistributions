2e8f293faf53764a5b7e0efc2190ce06101ce8d1
mansourr223 <hoodamansour2244@gmail.com>
2023-12-18 13:49:33 +0200

    edit code example

--- Continuous/ExampleThree.py
+++ Continuous/ExampleThree.py
@@ -1,13 +1,14 @@
 from scipy.stats import expon
 import numpy as np
-import matplotlib.pyplot as plt
+from plot import plot_pdf, plot_cdf
+
 
 n_ofEarthQuack = 20
 n_ofyears = 22
 mean = n_ofyears / n_ofEarthQuack  # lambda = 1/mean
 
 # What is the probability of the next earthquake in the next 6 months?
-time_period = 0.5  # 6 months
+time_period = 6  # 6 months
 x = 1 / (mean * 12)  # Rate lambda for 1 event per mean years
 probability_next_6_months = expon.pdf(time_period, scale=1 / x)
 
@@ -17,23 +18,9 @@ print("Probability of earthquake in the next 6 months:", probability_next_6_mont
 x_values = np.linspace(0, 5, 100)
 pdf_values = expon.pdf(x_values, scale=1 / x)
 cdf_values = expon.cdf(x_values, scale=1 / x)
+plot_pdf("Exponential", "PDF", x_values, pdf_values)
+plot_cdf("Exponential", "CDF", x_values, cdf_values)
 
-# Plot the PDF
-plt.figure(figsize=(12, 4))
-plt.subplot(1, 2, 1)
-plt.plot(x_values, pdf_values, label='PDF')
-plt.title('Exponential Distribution PDF')
-plt.xlabel('Time (years)')
-plt.ylabel('Probability Density')
-plt.legend()
 
-# Plot the CDF
-plt.subplot(1, 2, 2)
-plt.plot(x_values, cdf_values, label='CDF', color='orange')
-plt.title('Exponential Distribution CDF')
-plt.xlabel('Time (years)')
-plt.ylabel('Cumulative Probability')
-plt.legend()
 
-plt.tight_layout()
-plt.show()
+
