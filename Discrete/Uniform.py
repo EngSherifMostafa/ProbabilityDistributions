47398839bb7079acee1bbec4f04a6a5ae0ee74cd
ahmed-sherif24 <102686773+ahmed-sherif24@users.noreply.github.com>
2023-12-19 17:41:48 +0200

    Update Uniform.py

--- Discrete/Uniform.py
+++ Discrete/Uniform.py
@@ -1,51 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the interval [a, b]
+# Parameters
 a = 1
-b = 5
+b = 6
 
-# Generate uniform random variable
-uniform_data = np.random.uniform(a, b, size=1000)
+# Generate values for X using numpy.random.uniform
+values = np.random.uniform(a, b+1, 1000)  # Generating 1000 samples
 
 # Calculate mean and variance
-mean = np.mean(uniform_data)
-variance = np.var(uniform_data)
+mean = np.mean(values)
+variance = np.var(values)
+values = np.arange(a, b+1)
+pmf = np.ones_like(values) / (b - a + 1)
+# Plot PMF using plt.plot
+plt.plot(values, pmf, marker='o', linestyle='', color='blue')
+plt.title('Probability Mass Function (PMF) of X')
+plt.xlabel('X')
+plt.ylabel('P(X)')
 
-# Probability Mass Function (pmf)
-def uniform_pmf(x, a, b):
-    if a <= x <= b:
-        return 1 / (b - a)
-    else:
-        return 0
 
-# Cumulative Distribution Function (cdf)
-def uniform_cdf(x, a, b):
-    if x < a:
-        return 0
-    elif a <= x <= b:
-        return (x - a) / (b - a)
-    else:
-        return 1
-
-# Print mean and variance
-print("Mean:", mean)
-print("Variance:", variance)
-
-# Plot the histogram of the generated data
-plt.hist(uniform_data, bins=20, density=True, alpha=0.5, color='g', label='Histogram')
-
-# Plot the probability mass function (pmf)
-x_values = np.linspace(a - 1, b + 1, 100)
-pmf_values = [uniform_pmf(x, a, b) for x in x_values]
-plt.plot(x_values, pmf_values, 'r-', label='PMF')
-
-# Plot the cumulative distribution function (cdf)
-cdf_values = [uniform_cdf(x, a, b) for x in x_values]
-plt.plot(x_values, cdf_values, 'b-', label='CDF')
-
-plt.title('Uniform Distribution')
-plt.xlabel('Random Variable')
-plt.ylabel('Probability')
-plt.legend()
+#Plot PMF using plt.vlines
+plt.vlines(values, ymin=0, ymax=pmf, colors='blue', linestyle='-', linewidth=2)
+plt.title('Probability Mass Function (PMF) of X')
+plt.xlabel('X')
+plt.ylabel('P(X)')
 plt.show()
+
+
+# Display mean and variance
+print(f'Mean (μ): {mean}')
+print(f'Variance (σ^2): {variance}')
