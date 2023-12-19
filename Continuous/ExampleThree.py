dbd07453440df957be81712602b8b31cde3dcd7c
AhmedEslam152 <eslamahmed23468@gmail.com>
2023-12-19 04:17:42 +0200

    Update Example 3 code,Word

--- Continuous/ExampleThree.py
+++ Continuous/ExampleThree.py
@@ -3,16 +3,16 @@ import numpy as np
 from plot import plot_pdf, plot_cdf
 
 
-n_ofEarthQuack = 20
-n_ofyears = 22
-mean = n_ofyears / n_ofEarthQuack  # lambda = 1/mean
+n_ofEarthQuakes = 113    #Earthquakes happend from 1800 to 2023
+n_ofyears = 223   
+mean = n_ofEarthQuakes / n_ofyears  # lambda = 1/mean
 
 
-time_period = 6  # 6 months
-x = 1 / (mean * 12)  # Rate lambda for 1 event per mean years
-probability_next_6_months = expon.pdf(time_period, scale=1 / x)
+time_period = 50  # 50 years
+x = 1 / (mean)  # Rate lambda for 1 event per mean years
+probability_next_50_years = expon.pdf(time_period, scale=1 / x)
 
-print("Probability of earthquake in the next 6 months:", probability_next_6_months)
+print("Probability of earthquake in the next 50 years:", probability_next_50_years)
 
 # Plot the PDF and CDF for visualization
 x_values = np.linspace(0, 5, 100)
@@ -20,7 +20,3 @@ pdf_values = expon.pdf(x_values, scale=1 / x)
 cdf_values = expon.cdf(x_values, scale=1 / x)
 plot_pdf("Exponential", "PDF", x_values, pdf_values)
 plot_cdf("Exponential", "CDF", x_values, cdf_values)
-
-
-
-
